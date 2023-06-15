5cf8f6278b3fa56359802f3258ad1ae68af95414
Deimian Vásquez <deimianvasquez@gmail.com>
2023-06-14 20:46:38 -0400

    add todo list api

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Todos
 #from models import Person
 
 app = Flask(__name__)
@@ -16,7 +16,8 @@ app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,99 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    if request.method == "GET":
+        users = User.query.all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+        users = list(map(lambda item: item.serialize(), users))
+        new_users = []
 
-    return jsonify(response_body), 200
+        for user in users:
+            new_users.append(user.get("username"))
+    return jsonify(new_users), 200
+
+
+@app.route("/user/<string:username>", methods=["GET"])
+def get_user(username=None):
+    if request.method == "GET":
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            return jsonify({"msg": "user not found"}), 404
+        else:
+            todos = Todos.query.filter_by(user_id=user.id).all()
+            todos = list(map(lambda item: item.serialize(), todos))
+            return jsonify(todos), 200
+
+
+@app.route("/user/<string:username>", methods=["POST"])
+def create_user(username=None):
+    if request.method == "POST":
+        try:
+            data = request.json
+        except Exception as error:
+            return jsonify({"msg": "Debes enviar un array vacío"}), 500
+
+        if type(data) == list and len(data) == 0:
+            user = User.query.filter_by(username=username).first()
+            if user is not None:
+                return jsonify({"msg": "Usuario ya existe"}), 400
+            if user is None:
+                user = User(username=username)
+                db.session.add(user)
+                try:
+                    db.session.commit()
+
+                    todos = Todos(label="sample task",
+                                  done=False, user_id=user.id)
+                    db.session.add(todos)
+                    db.session.commit()
+                    return jsonify([]), 201
+                except Exception as error:
+                    print(error)
+                    return jsonify({"msg": error.args})
+
+
+@app.route("/user/<string:username>", methods=["PUT"])
+def update_task(username=None):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return jsonify({"msg": "user not found"}), 404
+
+    if user is not None:
+        todos = Todos.query.filter_by(user_id=user.id).all()
+        for todo in todos:
+            db.session.delete(todo)
+        db.session.commit()
+
+        data = request.json
+        if len(data) > 1:
+            for todo in data:
+
+                todo = Todos(label=todo["label"],
+                             done=todo["done"], user_id=user.id)
+                db.session.add(todo)
+            try:
+                db.session.commit()
+                return jsonify({"msg": f"Se guardaron {len(data)} tareas"}), 201
+            except Exception as error:
+                db.session.rollback()
+                return jsonify({"msg": error.args})
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,11 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    todos = db.relationship('Todos', backref='user', uselist=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +14,24 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(255), nullable=False)
+    done = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Todos %r>' % self.label
+
+    def serialize(self):
+        return {
+
+            "label": self.label,
+            "done": self.done
             # do not serialize the password, its a security breach
         }
